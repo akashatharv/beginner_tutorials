8adb093af44a4af766e08f01287303e5471ffa1b
akashatharv <akashatharv@gmail.com>
2018-11-13 16:41:21 -0500

    Implemented testing for the previously implemented ROS service "customString" using the gtest framework

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,57 @@
-
+/**=============================================================================
+ * @file       : test.cpp
+ * @author     : Akash Atharv
+ * @version    : 1.0
+ * @Copyright  : 3-Clause BSD
+Copyright (c) 2018, Akash Atharv
+ 
+Redistribution and use in source and binary forms, with or without  
+modification, are permitted provided that the following conditions are 
+met:
+ 
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+ 
+2. Redistributions in binary form must reproduce the above copyright 
+notice, this list of conditions and the following disclaimer in the   
+documentation and/or other materials provided with the distribution.
+ 
+3. Neither the name of the copyright holder nor the names of its 
+contributors may be used to endorse or promote products derived from this 
+software without specific prior written permission.
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+ * @brief      : Gtest implementation for custom ROS beginner-tutorials package 
+ *=============================================================================
+ */
 #include <gtest/gtest.h>
+#include <ros/ros.h>
+#include "beginner_tutorials/customString.h"
 
-TEST(TestSuite, testCase1)
-{
-<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
+TEST(TestSuite, ROS_SERVICE_CUSTOM_STRING_PASSES) {
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<beginner_tutorials::customString>(
+     "customString");
+  beginner_tutorials::customString srv;
+  srv.request.input = "Akash";
+  client.call(srv);
+  EXPECT_EQ(srv.response.output,"Akash");
 }
 
-int main(int argc, char **argv){
-  testing::InitGoogleTest(&argc, argv);
+int main(int argc, char **argv) {
   ros::init(argc, argv, "tester");
-  ros::NodeHandle nh;
+  testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
 
