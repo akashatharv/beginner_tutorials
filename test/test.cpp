183c81ac7e7b8969f9950910e225e43761bd40d8
akashatharv <akashatharv@gmail.com>
2018-11-13 14:59:43 -0500

    Rough test framework is written, sample test launch file is created and corresponding changes are made in CMakelists.txt

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+
+#include <gtest/gtest.h>
+
+TEST(TestSuite, testCase1)
+{
+<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "tester");
+  ros::NodeHandle nh;
+  return RUN_ALL_TESTS();
+}
+
